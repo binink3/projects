3fbb1b91c5edfa4647e9fdcd79f8782783516f7d
binink3 <48508684+binink3@users.noreply.github.com>
2020-01-15 19:09:23 +0900

    update

--- app.py
+++ app.py
@@ -16,11 +16,8 @@ def f_list_active_name():
     result = list(db.funds.find({'Status': "Active"}, {'_id': 0}))
     return jsonify(result)
 
-@app.route('/fund', methods=['GET'])  #검색창에 클라이언트가 입력을 하면, 그때 자동완성 List 띄어주는 API
-def f_list_active_info():
-    result = list(db.funds.find({'Status': "Active"}, {'_id': 0}))
 
-    return jsonify(result)
+#책임 운용사, 투자일, 회수일, 수익률 front ajax 들어감
 
 
 @app.route('/fund', methods=['POST']) #검색한 펀드의 운용역 이름이 포함된 List를 불러오는 API
