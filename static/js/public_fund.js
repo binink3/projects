9e19b996cb3f785dd646130256a053233ae3a136
binink3 <48508684+binink3@users.noreply.github.com>
2020-01-19 14:22:05 +0900

    current commit
    1. favicon  추가
    2. 과거 4개 보이는 부분 flex-wrap 처리로 4개씩 묶기 추가

--- static/js/public_fund.js
+++ static/js/public_fund.js
@@ -59,6 +59,81 @@ $(document).ready(function() {
 	});
 });
 
+function getfund(input){
+			let fund_name = $(input).text();
+			$("#fund-input").val(fund_name);
+}
+
+function setfund() {
+	let fund_name = $("#fund-input").val();
+	
+	// 클릭한 펀드의 정보 채우기
+	let this_fund_info;
+	for (let i = 0 ; i < all_fund_info.length; i++) {
+		if (all_fund_info[i]['fund_name'] == fund_name) {
+			this_fund_info = all_fund_info[i];
+		}
+	}
+
+	let company_name = this_fund_info["company_name"];
+	let manager_name = this_fund_info["manager_name"];
+	let fund_name_input = this_fund_info["fund_name"];
+	let start_date = this_fund_info["start_date"];
+	let end_date = this_fund_info["end_date"];
+	let fund_yield = this_fund_info["fund_yield"];
+
+	let fund_manager_current_info_wrap = '<div class="current-info-box">\
+											<div class="fund-info-current">\
+												<p>\
+												<b>'+fund_name_input+'</b> ('+company_name+')\
+												</p>\
+												<span class="fund-director">\
+													<b>책임 운용역 : </b>'+manager_name+'\
+												</span>\
+												<span class="fund-invested-date">\
+													<b>투자일 : </b>'+start_date+'\
+												</span>\
+												<span class="today-date">\
+													<b>기준일 : </b>'+end_date+'\
+												</span>\
+												<span class="fund-yield">\
+													<b>누적 수익률 : </b>'+fund_yield+'\
+												</span>\
+											</div>\
+										</div>'
+
+	$(".fund-manager-current-info-wrap").html(fund_manager_current_info_wrap);
+	$(".fund-manager-name > span").text(manager_name + " 펀드 매니저의 과거 Record Tracking 하기");
+
+	$.ajax({
+		type: "POST",
+		url: "/fund",
+		data: {fund_name_give:fund_name},
+		success: function(response){
+			$(".fund-track-records-wrap").empty();
+			for(let i = 0; i < response.length; i++){
+				let fund_name_db = response[i]["fund_name"];
+				let start_date = response[i]["start_date"];
+				let end_date = response[i]["end_date"];
+				let fund_yield = response[i]["fund_yield"];
+
+				let fund_track_record = '<div class="fund-track-record">\
+											<div class="record-info">\
+												<ul>\
+													<li class="fund-name-padding-bottom"><b>펀드명 :</b> ' +fund_name_db+'</li>\
+													<li><b>투자 날짜 :</b> ' +start_date+'</li>\
+													<li><b>회수 날짜 :</b> ' +end_date+'</li>\
+													<li><b>만기 수익률 :</b> ' +fund_yield+'</li>\
+												</ul>\
+											</div>\
+										</div>'
+				$(".fund-track-records-wrap").append(fund_track_record);
+			}
+			
+		}
+	});
+}
+
 
 
 
