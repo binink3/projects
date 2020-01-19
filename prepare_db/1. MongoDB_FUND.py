f3d4b4a5dcb99163c6736b3bab1109b7e9db0951
binink3 <48508684+binink3@users.noreply.github.com>
2020-01-19 13:09:27 +0900

    current commit
    1. 과거 없는 db 삭제
    2. css media query 시작

--- prepare_db/1. MongoDB_FUND.py
+++ prepare_db/1. MongoDB_FUND.py
@@ -12,7 +12,7 @@ file_list_xlsx = [file for file in file_list
                 if file.endswith(".xlsx")]
 
 for name in file_list_xlsx:
-    file_name = path+'/'+name
+    file_name = path+'\\'+name
     work_book = load_workbook(file_name)
     work_sheet = work_book['sheet']
 
@@ -42,7 +42,7 @@ for name in file_list_xlsx:
                 }
 
                 print (doc)
-                #db.funds.insert_one(doc)
+                db.funds.insert_one(doc)
 
 
     elif file_name.find('기준일') != -1:
@@ -67,4 +67,4 @@ for name in file_list_xlsx:
                 }
 
                 print(doc2) #사실은 doc2를 doc으로 하더라도 두번째 elif구문내에서 돌아가는 변수이기 때문에, 위의 doc 변수와 겹칠 일이 없다.
-                #db.funds.insert_one(doc2)
+                db.funds.insert_one(doc2)
