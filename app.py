cc6ea0687791e8ee9bc4be1035055f0ed62b625c
SunghoJeong <54521625+Sunghoyanolja@users.noreply.github.com>
2020-01-19 14:23:47 +0900

    2020.01.19 ver2

--- app.py
+++ app.py
@@ -11,16 +11,16 @@ def home():
    return render_template('public_fund.html')
 
 
-@app.route('/fund', methods=['GET'])  #검색창에 클라이언트가 입력을 하면, 그때 자동완성 List 띄어주는 API
+#검색창에 클라이언트가 입력을 하면, 그때 자동완성 List 띄어주는 API
+@app.route('/fund', methods=['GET'])
 def f_list_active_name():
     result = list(db.funds.find({'Status': "Active"}, {'_id': 0}))
     return jsonify(result)
 
 
-#책임 운용사, 투자일, 회수일, 수익률 front ajax 들어감
 
-
-@app.route('/fund', methods=['POST']) #검색한 펀드의 운용역 이름이 포함된 List를 불러오는 API
+#검색한 펀드의 운용역 이름이 포함된 List를 불러오는 API
+@app.route('/fund', methods=['POST'])
 def fm_past():
     fund_name_recieve = request.form['fund_name_give']
     target_fund_info = db.funds.find_one({'Status': "Active", 'fund_name':fund_name_recieve}, {'_id': 0})
@@ -29,8 +29,10 @@ def fm_past():
 
     return jsonify(funds_info)
 
+#Port를 설정하는 Coding
 if __name__ == '__main__':
    app.run('0.0.0.0',port=5000,debug=True)
 
 
+#책임 운용사, 투자일, 회수일, 수익률 front ajax 들어감
 
